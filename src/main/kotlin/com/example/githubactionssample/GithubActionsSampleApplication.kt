f231ea4a08aa8432a8f9d7245381cb164da900b8
mpaszkowski <paszkowski.mikolaj98@gmail.com>
2024-04-11 12:55:00 +0200

    initial Kotlin + Gradle project

---
+++ src/main/kotlin/com/example/githubactionssample/GithubActionsSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.githubactionssample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GithubActionsSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<GithubActionsSampleApplication>(*args)
+}
